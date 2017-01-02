cadffccc09fde85976539035257685c77e3243ea
iftekar <hiifte@gmail.com>
2017-01-02 15:59:03 +0530

    3.58

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works :) !';
 }
---
+++ src/app/routes.ts
@@ -0,0 +1,22 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent }  from './home/home.component';
+//import { ContactComponent }  from './contact/contact.component';
+//import { AppAbout }  from './pages/about/app.about';
+
+const appRoutes: Routes = [
+    // { path: '/**',component: AppComponent},
+    //{ path: '/*',component: AppComponent},
+    { path: '', component: HomeComponent},
+    //{ path: 'contact', component: ContactComponent},
+
+
+
+
+];
+
+
+export const appRoutingProviders: any[] = [
+];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes,{ useHash: true });
